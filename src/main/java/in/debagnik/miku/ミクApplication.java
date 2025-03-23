dc7b631bc589ea8cab6ab09831ff6c2aaeac152f
Debagnik <debagnikkar@outlook.com>
2025-03-24 05:07:04 +0530

    Initial Project Creation

---
+++ src/main/java/in/debagnik/miku/ミクApplication.java
@@ -0,0 +1,13 @@
+package in.debagnik.miku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ミクApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ミクApplication.class, args);
+	}
+
+}
