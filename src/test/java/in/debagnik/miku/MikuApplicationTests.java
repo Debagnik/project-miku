c37b9cce12190dceebc9365f340cfb64fde57a82
Debagnik <debagnikkar@outlook.com>
2025-03-25 23:29:46 +0530

    Project Initilaization

---
+++ src/test/java/in/debagnik/miku/MikuApplicationTests.java
@@ -0,0 +1,13 @@
+package in.debagnik.miku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
